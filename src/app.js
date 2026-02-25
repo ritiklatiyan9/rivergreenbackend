114ed1e2a429669aada9fffebf975c6d2d270e9e
[Ritik] <ritikkumar8077008067@gmail.com>
2026-02-25 17:24:12 +0530

    t

--- src/app.js
+++ src/app.js
@@ -10,7 +10,11 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan('combined'));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
